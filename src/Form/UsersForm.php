3a339b673fbc6ed3476a202f6198ca310e51deef
anmol goel <anmolgoyal74@gmail.com>
2018-03-05 17:30:46 +0530

    Invitation

--- src/Form/UsersForm.php
+++ src/Form/UsersForm.php
@@ -1,72 +1,101 @@
-  <?php
+<?php
+/**
+ * @file
+ * Contains \Drupal\openlms_users\Form\UsersForm.
+ */
+namespace Drupal\openlms_users\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Database\Database;
+use Drupal\Core\Entity;
+use Drupal\Core\Access\AccessibleInterface;
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
+class UsersForm extends FormBase {
   /**
-   * @file
-   * Contains \Drupal\openlms_users\Form\UsersForm.
+   * {@inheritdoc}
    */
-  namespace Drupal\openlms_users\Form;
-  use Drupal\Core\Form\FormBase;
-  use Drupal\Core\Form\FormStateInterface;
-  class UsersForm extends FormBase {
-    /**
-     * {@inheritdoc}
-     */
-    public function getFormId() {
-      return 'uid';
-    }
-
-      /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state, $uid= NULL) 
-
-    {
-      
-      
-
-      foreach ( $data as $key=>$value )
-      { 
-        $form[$value[0]] = array (
-        '#type' => 'radios',
-        '#title' => $key,
-        '#options' => $value[1],
-      );
-
-
-      }
-      $form['qid'] = array (
-        '#type' => 'hidden',
-        '#value' => $qid,
-      );
-
-      $form['actions']['#type'] = 'actions';
-      $form['actions']['submit'] = array(
-        '#type' => 'submit',
-        '#value' => $this->t('Submit'),
-        '#button_type' => 'primary',
-      );
-      return $form;
-    }
-
-   /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-     
-  $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-      foreach ($form_state->getValues() as $key => $value) {
-        if(is_numeric($key))
-        db_insert('openlms_quiz')
-      ->fields(array(
-        'uid' => $user->id(),
-        'qid' => $form_state->getValue('qid'),   
-        'question' => $key, 
-        'Response' => $value+1, 
-        'Submitted' => time(), 
-      ))->execute();
-      drupal_set_message("successfully Submitted the responses");
-      }
-     
-      
+  public function getFormId() {
+    return 'openlms_user_form';
   }
 
-     }
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) 
+
+  {
+    $nids = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'group']);
+    $titles=array();
+    foreach ($nids as $key=>$value) {
+      $nodes=\Drupal\node\Entity\Node::load($key);
+      foreach ($nodes as $keys=>$values) {
+        $title=$nodes->get('title')->getValue()[0]['value'];
+        $invite_code=$nodes->get('field_invite_code')->getValue()[0]['value'];
+      } 
+      $titles[$invite_code]=$title;
+
+    }
+    $form['group_name'] = array(
+      '#type' => 'select',
+      '#options' => $titles,
+      '#title' => 'A dropdown menu for selecting groups.',
+      );
+    $form['emails'] = array(
+      '#type' => 'textarea',
+      '#title' => 'Please enter line separated email ids',
+      '#required' => TRUE,  
+
+      );
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+      '#button_type' => 'primary',
+      );
+    return $form;
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $emails = $form_state->getValue('emails');
+    $array = [];
+    $array = explode("\n",$emails);
+    foreach ($array as $email) {    
+      $email = trim($email);
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $form_state->setErrorByName('Email', $this->t('Please enter valid list of Email IDs.  "'.$email.'" is not in right format')); 
+      }
+    }
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $emails = $form_state->getValue('emails');
+    $group_name = $form_state->getValue('group_name');
+   
+    $array = [];
+    $array = explode("\n",$emails);
+     $mailManager = \Drupal::service('plugin.manager.mail');
+ $module = 'openlms_users';
+ $key = 'invitation';
+ $to = \Drupal::currentUser()->getEmail();
+ $params['message'] = "This is the Body of the mail".$group_name;
+ $params['node_title'] = "Invitation to join the group";
+ $langcode = \Drupal::currentUser()->getPreferredLangcode();
+ $send = true;
+ $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+ if ($result['result'] !== true) {
+   drupal_set_message(t('There was a problem sending your message and it was not sent.'), 'error');
+ }
+ else {
+   drupal_set_message(t('Your message has been sent.'));
+ } 
+  }
+}
